5cba2716b2ac8be32cc084b8fd5f1e04831fc8e7
Siwoo Chung <siwoochung@me.com>
2020-12-17 14:27:52 +0900

    Apply changes to workload

--- download_workload.sh
+++ download_workload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O workload.txt "https://cdn-std.droplr.net/files/acc_601378/JdaTkn?download&response-content-disposition=attachment%3B%20filename%3Dworkload.txt"
+wget -O workload.txt "https://cdn-std.droplr.net/files/acc_601378/gyBnXf?download&response-content-disposition=attachment%3B%20filename%3Dworkload.txt"
